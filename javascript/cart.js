d22131b3b2245d80fe61d859f5d961dffddca35c
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-08-23 12:50:07 +0530

    m

---
+++ javascript/cart.js
@@ -0,0 +1,36 @@
+import navbar from "../componets/nav.js";
+document.getElementById("navbar").innerHTML = navbar();
+
+
+const ui = (data) => {
+    data.map((prodcut) => {
+      if (prodcut.catagary == "gift plants") {
+        let img1 = document.createElement("img")
+        img1.src = prodcut.img1
+        let title = document.createElement("p")
+        title.innerHTML = prodcut.title
+        let selling_price = document.createElement("p")
+        selling_price.innerHTML = prodcut.selling_price
+        let orginal_price = document.createElement("p")
+        orginal_price.innerHTML = prodcut.orginal_price
+        let btn = document.createElement("button")
+        btn.innerHTML = "buy now"
+        let div = document.createElement("div")
+        div.append(title, selling_price, orginal_price, btn)
+        div.classList.add("col-9","p-4")
+        let div2 = document.createElement("div")
+        div2.append(img1,div)
+        div2.classList.add("col-12","row","shadow","p-0")
+        document.getElementById("cart").append(div2)
+      }
+    })
+  };
+  
+  let get = async () => {
+    let res = await fetch("http://localhost:3000/cart");
+    let data = await res.json();
+    ui(data);
+    console.log(data)
+  };
+  
+  get();
